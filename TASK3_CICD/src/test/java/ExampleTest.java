6db8c8c065c1794e48b1a7400a9746d87b616d7a
pratikgole2004 <mr.pgole@gmail.com>
2025-07-19 11:18:39 +0530

    Add files via upload

---
+++ TASK3_CICD/src/test/java/ExampleTest.java
@@ -0,0 +1,12 @@
+package test.java;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ExampleTest {
+
+    @Test
+    public void testAddition() {
+        assertEquals(4, 2 + 2);
+    }
+}
